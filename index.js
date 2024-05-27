6d2daf0236c824856084e55af0bcdf7ec3c6ca23
Adin <shahabadin25@outlook.com>
2024-05-28 02:57:12 +0530

    Added Preflight Request Options

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const corsOption = {
 
 
 app.use(cors(corsOption));
+app.options("*",cors(corsOption))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
